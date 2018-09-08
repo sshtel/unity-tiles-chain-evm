405518a0d8692a8a45d02a4272250669d16cf0f0
Steve Song <sshtel@gmail.com>
2018-09-08 22:38:38 +0900

    feat: createQuest, startQuest, rand

--- dappchain/contracts/legendary.sol
+++ dappchain/contracts/legendary.sol
@@ -9,74 +9,92 @@ contract LegendQuest is Ownable {
   using SafeMath32 for uint32;
   using SafeMath16 for uint16;
 
-  event NewQuest(uint uid, string title);
+  event NewQuest(bool success, uint qid);
+  event NewUser(uint uid, string title);
+  event StartQuest(uint questSlotId);
 
   uint cooldownTime = 30 seconds;
 
   struct Quest {
-    uint key;
-    string title;
+    uint inUse;
+    uint qid;
     uint periodTime;
     uint endTime;
     // Party MyParty;
     // Monster MyMonster;
-
-    string monsterName;
-    // string monsterThumbImageId;
-    uint monsterPower;
-    // uint[] Normal_Trophy;    //2
-    // uint[] Random_Trophy;    //6
-    // uint Trophy_Count_Possible;
-    // bool[] Trophy_Checked;
-    // uint Trophy_Count_Selected;
   }
 
   struct User {
     string username;
   }
 
-  Quest[] public quests;
   User[] public users;
+  uint public userCount = 0;
 
   mapping (address => uint) public ownerUid;      // msg.sender : uid(idx)
-  mapping (uint => address) public questToOwner;  // rand Key : msg.sender
-  mapping (address => uint) ownerQuestCount;      // msg.sender : number of quests
+  mapping (address => uint) public ownerQuestCount;      // msg.sender : number of quests
 
-  function createQuest(string title, uint periodTime, string monsterName, uint monsterPower) public onlyOwner {
-    uint randKey = uint(keccak256(now, title));
-    uint endTime = now + periodTime;
-    uint id = quests.push(Quest(randKey, title, periodTime, endTime, monsterName, monsterPower)) - 1;
-    questToOwner[randKey] = msg.sender;
-    emit NewQuest(id, title);
+  mapping (address => Quest) public quest1;
+  mapping (address => Quest) public quest2;
+  mapping (address => Quest) public quest3;
+  mapping (address => Quest) public quest4;
+  
+
+  function createQuest(uint qid, uint periodTime) public {
+    if (quest1[msg.sender].inUse == 0) {
+      quest1[msg.sender].inUse = 1;
+      quest1[msg.sender].qid = qid;
+      quest1[msg.sender].periodTime = periodTime;
+    } else if (quest2[msg.sender].inUse == 0) {
+      quest2[msg.sender].inUse = 1;
+      quest2[msg.sender].qid = qid;
+      quest2[msg.sender].periodTime = periodTime;
+    } else if (quest3[msg.sender].inUse == 0) {
+      quest3[msg.sender].inUse = 1;
+      quest3[msg.sender].qid = qid;
+      quest3[msg.sender].periodTime = periodTime;
+    } else if (quest4[msg.sender].inUse == 0) {
+      quest4[msg.sender].inUse = 1;
+      quest4[msg.sender].qid = qid;
+      quest4[msg.sender].periodTime = periodTime;
+    } else {
+      emit NewQuest(false, qid); //fail
+    }
+    emit NewQuest(true, qid);
   }
 
-  function openRandomQuest(uint _uid) public returns (uint, uint) {
-    uint rand = uint(keccak256(now, _uid));
-    uint questRandModulus = quests.length;
-    uint randQid = rand % questRandModulus;
-    ownerQuestCount[msg.sender] = ownerQuestCount[msg.sender].add(1);
-    return (_uid, randQid);
+  function startQuest(uint questSlotId) public returns (uint, uint) {
+    if (questSlotId == 1) {
+      quest1[msg.sender].endTime = now + quest1[msg.sender].periodTime;
+      StartQuest(questSlotId);
+    } else if (questSlotId == 2) {
+      quest2[msg.sender].endTime = now + quest2[msg.sender].periodTime;
+      StartQuest(questSlotId);
+    } else if (questSlotId == 3) {
+      quest3[msg.sender].endTime = now + quest3[msg.sender].periodTime;
+      StartQuest(questSlotId);
+    } else if (questSlotId == 4) {
+      quest4[msg.sender].endTime = now + quest4[msg.sender].periodTime;
+      StartQuest(questSlotId);
+    }
   }
 
-  function createUser(string username) public returns (uint) {
+  function createUser(string username) public {
     users.push(User(username));
-    uint id = users.length - 1;
-    ownerUid[msg.sender] = id;
-    return id;
+    ownerUid[msg.sender] = userCount;
+    userCount++;
+    uint idx = userCount - 1;
+    emit NewUser(idx, username);
   }
 
-  function getUserInfo(address owner) public view returns (string) {
-    uint uid = ownerUid[owner];
+  function getUserInfo() public view returns (string) {
+    uint uid = ownerUid[msg.sender];
     User storage user = users[uid];
     return user.username;
   }
 
   function getUserCount() public constant returns(uint) {
       return users.length;
-  }
-
-  function getQuestCount() public constant returns(uint) {
-      return quests.length;
   }
 
   function whoAmI() public view returns (address) {
@@ -87,5 +105,9 @@ contract LegendQuest is Ownable {
     return 1;
   }
 
+  function rand(uint modulus) public returns (uint) {
+    uint rand = uint(keccak256(now, msg.sender));
+    return rand % modulus;
+  }
 
 }
